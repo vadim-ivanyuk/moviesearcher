b8917114ba56cedacd15f7899abe2ba16cc69c11
vadim-ivanyuk <vadim.ivanyuk.621@gmail.com>
2021-03-21 22:44:22 +0200

    Updates

---
+++ precache-manifest.7c42e57acf4033a38f8099454874ec27.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37885fa93865d5087a6ca17d9d01278d",
+    "url": "/moviesearcher/index.html"
+  },
+  {
+    "revision": "273c1512e1317a10b0e0",
+    "url": "/moviesearcher/static/css/2.ff645aaf.chunk.css"
+  },
+  {
+    "revision": "da971d6d1d520a80b4b9",
+    "url": "/moviesearcher/static/css/main.7c93868a.chunk.css"
+  },
+  {
+    "revision": "273c1512e1317a10b0e0",
+    "url": "/moviesearcher/static/js/2.c74a2187.chunk.js"
+  },
+  {
+    "revision": "6e3c36ff131faa785f737282b549d40b",
+    "url": "/moviesearcher/static/js/2.c74a2187.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da971d6d1d520a80b4b9",
+    "url": "/moviesearcher/static/js/main.aa43439c.chunk.js"
+  },
+  {
+    "revision": "d55dc94b073a7b6be138",
+    "url": "/moviesearcher/static/js/runtime-main.2fc7d061.js"
+  },
+  {
+    "revision": "3ebae8b0304e91e9b16eca8e9c4d2464",
+    "url": "/moviesearcher/static/media/not-found-backdrop.3ebae8b0.jpg"
+  },
+  {
+    "revision": "491cf8e5c4777336e0418a56350031e2",
+    "url": "/moviesearcher/static/media/not-found-poster.491cf8e5.jpg"
+  }
+]);
